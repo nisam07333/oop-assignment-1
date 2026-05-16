e7edb27f3e667f3c9692a9179af2f7c94bee63ed
nisam07333 <thebenisam5@gmail.com>
2026-05-16 10:15:05 +0545

    added assignments files
    tutorial 1 assignment submission

---
+++ OperatorsProgram.java
@@ -0,0 +1,22 @@
+public class OperatorsProgram {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        // Arithmetic Operators
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Substraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+        // Relational Operators
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        // Logical Operators
+        System.out.println("(a > 0 && b > o): " + (a > 0 && b > 0));
+        System.out.println("(a > 0 || b < 0) : " + (a > 0 || b < 0));
+        System.out.println("!(a < b) : " + !(a < b));
+    }
+}
+
---
+++ VariablesandDatatypes.java
@@ -0,0 +1,15 @@
+public class VariablesandDatatypes {
+    public static void main(String[] args) {
+
+        int age = 25;
+        double salary = 2500.50;
+        boolean isStudent = true;
+        char grade = 'A';
+        String name = "Sita";
+        System.out.println("Integer value: "+age);
+        System.out.println("Double value: "+salary);
+        System.out.println("Boolean value: + isStudent");
+        System.out.println("String value: "+name);
+    }
+}
+
